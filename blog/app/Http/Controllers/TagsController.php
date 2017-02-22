162f27545d574a05c598e4d5a81716f5532da18e
Ricardo <ricardogauto11@gmail.com>
2017-02-21 22:31:48 -0300

    Creando los métodos: create, edit, update, index

--- blog/app/Http/Controllers/TagsController.php
+++ blog/app/Http/Controllers/TagsController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\TagRequest;
+use App\Tag;
+use Laracasts\Flash\Flash;
 
 class TagsController extends Controller
 {
@@ -11,9 +14,10 @@ class TagsController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        return view('admin.tags.index');
+        $tags = Tag::search($request->name)->orderBy('id', 'DESC')->paginate(5);
+        return view('admin.tags.index', ['tags' => $tags]);
     }
 
     /**
@@ -32,9 +36,13 @@ class TagsController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(TagRequest $request)
     {
-        //
+        $tag = new Tag($request->all());
+        $tag->save();
+
+        Flash::success('El tag ' . $tag->name . ' ha sido agregado correctamente!');
+        return redirect()->route('tags.index');
     }
 
     /**
@@ -56,7 +64,9 @@ class TagsController extends Controller
      */
     public function edit($id)
     {
-        //
+        $tag = Tag::find($id);
+
+        return view('admin.tags.edit', ['tag' => $tag]);
     }
 
     /**
@@ -68,7 +78,13 @@ class TagsController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $tag = Tag::find($id);
+        $tag->fill($request->all());
+        $tag->save();
+
+        Flash::warning('El tag ' . $tag->name . ' ha sido editado correctamente!');
+
+        return redirect()->route('tags.index');
     }
 
     /**
@@ -79,6 +95,10 @@ class TagsController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $tag = Tag::find($id);
+        $tag->delete();
+
+        Flash::error('El tag ' . $tag->name . ' ha sido borrado!');
+        return redirect()->route('tags.index');
     }
 }
